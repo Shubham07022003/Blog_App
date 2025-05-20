b10f8e782d293269b7d9355a400848e191a1985a
syntaxSmith_07 <kumarshubham07022003@gmail.com>
2025-05-20 18:49:11 +0530

    env change

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
 module.exports = {
-  host: process.env.DB_HOST || 'localhost',
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'blog_db',
+  host: process.env.MYSQLHOST || 'localhost',
+  username: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || '',
+  database: process.env.MYSQLDATABASE || 'blog_db',
   dialect: 'mysql',
   logging: false,
 }; 
